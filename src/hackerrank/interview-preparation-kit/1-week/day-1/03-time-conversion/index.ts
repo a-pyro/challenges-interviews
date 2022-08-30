a18587e9e5cc2aa39ff9ec7f4bd57eb8043bab52
Ardi Germenji <ardigermenji@MacBook-Air-di-Ardi.local>
2022-08-30 16:07:48 -0400

    feat(hack-int-1w-d1-03): adds time conversion

---
+++ src/hackerrank/interview-preparation-kit/1-week/day-1/03-time-conversion/index.ts
@@ -0,0 +1,22 @@
+const mapPMto24h = {
+  '01': '13',
+  '02': '14',
+  '03': '15',
+  '04': '16',
+  '05': '17',
+  '06': '18',
+  '07': '19',
+  '08': '20',
+  '09': '21',
+  '10': '22',
+  '11': '23',
+  '12': '12',
+}
+
+const timeConversion = (s: string): string => {
+  const partOfTheDay = s.slice(-2)
+  const [hours, minutes, seconds] = s.slice(0, -2).split(':')
+  return `${
+    partOfTheDay === 'PM' ? mapPMto24h[hours as keyof typeof mapPMto24h] : hours
+  }:${minutes}:${seconds}`
+}
